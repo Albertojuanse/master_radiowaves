cbc965145930430bd6990f4ac3cd3a784318a65d
Alberto Juan Sebastian <albertojuansebastian@gmail.com>
2019-10-12 17:26:59 +0200

    minor errors

--- Circuit analysis/Circuits_ABCD/ABCDtoS.m
+++ Circuit analysis/Circuits_ABCD/ABCDtoS.m
@@ -7,10 +7,10 @@ A=ABCD(1,1); B=ABCD(1,2); C=ABCD(2,1); D=ABCD(2,2);
 
 % Conversion
 denom =   A * Z02 + B + C *      Z01  * Z02 + D * Z01;
-S11   = ( A * Z02 + B - C * conj(Z01) * Z02 - D * conj(Z01)) / denom;
-S12   =   2 * sqrt( real(Z01) * real(Z02)) * (A*D-B*C)       / denom;
-S21   =   2 * sqrt( real(Z01) * real(Z02))                   / denom;
-S22   = (-A * conj(Z02) + B - C * conj(Z02) * Z01 + D * Z01) / denom;
+S11   = (( A * Z02 + B - C * conj(Z01) * Z02 - D * conj(Z01))) / denom;
+S12   = (  2 * sqrt( real(Z01) * real(Z02)) * (A*D-B*C)      ) / denom;
+S21   = (  2 * sqrt( real(Z01) * real(Z02))                  ) / denom;
+S22   = ((-A * conj(Z02) + B - C * conj(Z02) * Z01 + D * Z01)) / denom;
 
 % Compose result
 S = [S11 S12; S21 S22];
