16116ac76fed6f9a12f36e2673af9f545a38f673
Alberto Juan Sebastian <albertojuansebastian@gmail.com>
2019-10-20 17:44:06 +0200

    Circuit analysis. Exercise 1 y 2

--- Circuit analysis/Exercises/Ex1_CascadeTwoSmatrices.m
+++ Circuit analysis/Exercises/Ex1_CascadeTwoSmatrices.m
@@ -1,15 +1,15 @@
-function S = Ex1_CascadeTwoSmatrices()
-%EX1_CASCADETWOSMATRICES
-%   Implemente una función que tome como entrada dos matrices de parámetros
-%   S de dos cuadripolos referidas a la misma impedancia, y devuélva la 
-%   matriz S de la concatenación de ambos cuadripolos.
+%% Exercise 1
+% Implemente una función que tome como entrada dos matrices de parámetros
+% S de dos cuadripolos referidas a la misma impedancia, y devuélva la
+% matriz S de la concatenación de ambos cuadripolos.
 
 % Declarations
-S1 = [0.5 0.5; 0.5 0.5];
+S1 = [0.5 0.5; 0.5 0.5];    % First matrix
 Z011 = 50;
 Z012 = 50;
-S2 = [0.5 0.5; 0.5 0.5];
+S2 = [0.5 0.5; 0.5 0.5];    % Second matrix
 Z021 = 50;
 Z022 = 50;
-S = cascadeS(S1, Z011, Z012, S2, Z021, Z022);
-end
+
+% Call the function
+S = cascadeS(S1, Z011, Z012, S2, Z021, Z022)
--- Circuit analysis/Exercises/Ex2_MatricesConversions.m
+++ Circuit analysis/Exercises/Ex2_MatricesConversions.m
@@ -1,15 +1,13 @@
-function S = Ex2_MatricesConversions()
-%EX2_MATRICESCONVERSIONS Summary of this function goes here
-%   Implemente una función que tome como entrada dos matrices de parámetros
-%   S de un cuadripolo y su impedancia de referencia, y devuelva la matriz 
-%   de parámetros ABCD. Implemente la función inversa.
+%% Exercise 2
+% Implemente una función que tome como entrada una matriz de parámetros S
+% de un cuadripolo y su impedancia de referencia, y devuelva la matriz de
+% parámetros ABCD. Implemente la función inversa.
 
 % Declaration
-S0 = [0.5 0.5; 0.5 0.5];
+S0 = [0.5 0.5; 0.5 0.5]
 Z01 = 50;
 Z02 = 50;
 
 % Conversions
-ABCD = StoABCD(S0, Z01, Z02);
-S = ABCDtoS(ABCD, Z01, Z02);
-end
+ABCD = StoABCD(S0, Z01, Z02)
+S = ABCDtoS(ABCD, Z01, Z02)
