893c22d5d42b6060ca943172fe4052c31b2448a5
Alberto Juan Sebastian <albertojuansebastian@gmail.com>
2019-10-20 14:50:41 +0200

    Filtering. Exercise 1

--- Filtering/Exercises/Ex1_TheorycalFiltersResponse.m
+++ Filtering/Exercises/Ex1_TheorycalFiltersResponse.m
@@ -1,4 +1,3 @@
-
 %% Exercise 1
 % Representar sobre una misma figura las respuestas teóricas (la expresión
 % teórica del parámetro S21 en dB en función del polinomio correspondiente)
@@ -11,35 +10,102 @@
 % simplemente mostrando el detalle de la banda de paso con un rango de 0.5 
 % dB. El eje de abscisas en los dos casos siempre será la frecuencia en Hz.
 
+%% Fisrt Draw
 % Butterworth filters
 m = 1000;               % Number of samples
-n = 1:5;                % Order
-pulsation_max = 2 * pi; % Max pulsation in arrays
+n = 5;                  % Orders
+pulsation_max = 2;      % Max pulsation in arrays
 
 for i_n = n
     
     [pulsation, module, phase] = LowPassPrototipeButterworthFilter(i_n, m, pulsation_max);
-    plot(pulsation, module)
+    plot(pulsation/2*pi, 20*log(module))
     hold on;
 
 end
 
 % Chebychev filters
-m = 1000;               % Number of samples
-n = 1:5;                % Order
-pulsation_max = 2 * pi; % Max pulsation in arraysn = 1:5;
-epsilon = 1;            % Epsilon for ripple
+m = 1000;                               % Number of samples
+n = 5;                                  % Order
+pulsation_max = 2 ;                     % Max pulsation in arraysn = 1:5;
+
+epsilon = sqrt(10^(0.1/10) -1);         % Epsilon for 0.1 dB ripple
 
 for i_n = n
 
-    [pulsation, module, phase] = LowPassPrototipeChebychevFilter(i_n, m, epsilon, pulsation_max);
-    plot(pulsation, module)
+    [pulsation, module, phase] = LowPassPrototipeChebychevFilter(i_n, m, pulsation_max, epsilon);
+    plot(pulsation/2*pi, 20*log(module))
     hold on;
 
 end
 
-% Plot configurationliou
+epsilon = sqrt(10^(0.01/10) -1);         % Epsilon for 0.01 dB ripple
 
-xlim([0 1]);
-ylim([0 3]);
+for i_n = n
+
+    [pulsation, module, phase] = LowPassPrototipeChebychevFilter(i_n, m, pulsation_max, epsilon);
+    plot(pulsation/2*pi, 20*log(module))
+    hold on;
+
+end
+
+% Plot configuration
+%cut_amplitude = 1+epsilon^2;
+%cut_amplitude_str = strcat('1+k²=', num2str(cut_amplitude));
+% annotation('textarrow',[1/pulsation_max 1/pulsation_max], [1/pulsation_max 1/pulsation_max],'String',cut_amplitude_str)
+title('Respuesta teórica maximalmente plana y equirizada de orden 5');
+xlim([0 pulsation_max/2*pi]);
+ylim([-60 0]);
+xlabel('Frecuencia (Hz)');
+ylabel('Módulo (dB)');
+hold off;
+
+%% Second draw
+% Butterworth filters
+m = 1000;               % Number of samples
+n = 5;                  % Orders
+pulsation_max = 2;      % Max pulsation in arrays
+
+for i_n = n
+    
+    [pulsation, module, phase] = LowPassPrototipeButterworthFilter(i_n, m, pulsation_max);
+    plot(pulsation/2*pi, 20*log(module))
+    hold on;
+
+end
+
+% Chebychev filters
+m = 1000;                               % Number of samples
+n = 5;                                  % Order
+pulsation_max = 2 ;                     % Max pulsation in arraysn = 1:5;
+
+epsilon = sqrt(10^(0.1/10) -1);         % Epsilon for 0.1 dB ripple
+
+for i_n = n
+
+    [pulsation, module, phase] = LowPassPrototipeChebychevFilter(i_n, m, pulsation_max, epsilon);
+    plot(pulsation/2*pi, 20*log(module))
+    hold on;
+
+end
+
+epsilon = sqrt(10^(0.01/10) -1);         % Epsilon for 0.01 dB ripple
+
+for i_n = n
+
+    [pulsation, module, phase] = LowPassPrototipeChebychevFilter(i_n, m, pulsation_max, epsilon);
+    plot(pulsation/2*pi, 20*log(module))
+    hold on;
+
+end
+
+% Plot configuration
+%cut_amplitude = 1+epsilon^2;
+%cut_amplitude_str = strcat('1+k²=', num2str(cut_amplitude));
+% annotation('textarrow',[1/pulsation_max 1/pulsation_max], [1/pulsation_max 1/pulsation_max],'String',cut_amplitude_str)
+title({'Respuesta teórica maximalmente plana y equirizada de orden 5', 'Detalle de la banda de paso.'});
+xlim([0 pulsation_max/2*pi-1.3]);
+ylim([-0.5 0]);
+xlabel('Frecuencia (Hz)');
+ylabel('Módulo (dB)');
 hold off;
--- Filtering/LowPassPrototipes/LowPassPrototipeChebychevFilter.m
+++ Filtering/LowPassPrototipes/LowPassPrototipeChebychevFilter.m
@@ -7,6 +7,6 @@ pulsation = linspace(0, pulsation_max, m);
 
 % The theorical response of a Butterwirth filter is
 phase = zeros(size(pulsation, 2));
-module = 1 ./ ( 1 + epsilon^2 * cosh( n * acosh(pulsation) ) );
+module = 1 ./ ( 1 + epsilon^2 .* (cosh( n * acosh(pulsation) ) ).^2 );
 
 end
