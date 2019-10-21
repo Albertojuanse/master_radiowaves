ea76e540956773bd69db7cc240441263cf230092
Alberto Juan Sebastian <albertojuansebastian@gmail.com>
2019-10-21 10:48:42 +0200

    isSimetrical

--- Circuit analysis/Matrices_S/isSimetrical.m
+++ Circuit analysis/Matrices_S/isSimetrical.m
@@ -1,9 +1,10 @@
-function res = isSimetrical(S)
+function [res, simetries] = isSimetrical(S)
 %ISSIMETRICAL Return true if tyhe given S matrix's system is simetrical in
 %any port
 
 % If it is simetrical, some Sii parameter is equals to other Sjj
 res = false;
+simetries = {};
 for i_s = 1:size(S, 2)
     for j_s = 1:size(S, 2)
         
@@ -12,7 +13,8 @@ for i_s = 1:size(S, 2)
             
             % Compare each S parameter with others
             if S(i_s, i_s) == S(j_s, j_s)
-                res = true;                
+                res = true;
+                simetries{1, end+1} = [i_s j_s];
             end
             
         end
