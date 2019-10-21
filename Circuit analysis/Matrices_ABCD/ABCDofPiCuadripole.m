7721293c1d0377f8618dc1e5881770e6b1a19a9f
Alberto Juan Sebastian <albertojuansebastian@gmail.com>
2019-10-21 09:52:36 +0200

    References

--- Circuit analysis/Matrices_ABCD/ABCDofPiCuadripole.m
+++
@@ -1,7 +0,0 @@
-function ABCD = ABCDofPiCuadripole(Y1, Y2, Y3)
-%ABCDOFPICUADIPOLE This function calculates de ABCD matrix of a Pi type
-% cuadripole, given its admitances
-% Pozar p. 185
-
-% Compose de solution
-ABCD = [(1+Y2/Y3) (1/Y3); (Y1+Y2+Y1*Y2/Y3) (1+Y1/Y4)];
--- Circuit analysis/Matrices_ABCD/ABCDofTCuadripole.m
+++
@@ -1,7 +0,0 @@
-function ABCD = ABCDofTCuadripole(Z1, Z2, Z3)
-%ABCDOFTCUADRIPOLE This function calculates de ABCD matrix of a T type
-%cuadripole, given its impedances
-% Pozar p. 185
-
-% Compose de solution
-ABCD = [(1+Z1/Z3) (Z1+Z2+Z1*Z2/Z3); (1/Z3) (1+Z2/Z3)];
